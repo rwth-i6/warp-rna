f16e7f485fc48c8eb78d61409a31df65af5045e6
Andre Merboldt <spotlight0xff@gmail.com>
2020-10-22 22:53:35 +0200

    tensorflow binding

---
+++ tensorflow_binding/src/warp_rna_op.cc
@@ -0,0 +1,156 @@
+#ifdef WARPRNA_ENABLE_GPU
+#define EIGEN_USE_GPU
+#include <cuda.h>
+#endif
+
+#include "tensorflow/core/framework/op.h"
+#include "tensorflow/core/framework/op_kernel.h"
+#include "tensorflow/core/framework/bounds_check.h"
+#include "tensorflow/core/framework/shape_inference.h"
+#include "core.h"
+
+namespace tf = tensorflow;
+
+
+REGISTER_OP("WarpRNA")
+    .Input("log_probs: float32")
+    .Input("labels: int32")
+    .Input("input_lengths: int32")
+    .Input("label_lengths: int32")
+    .Attr("blank_label: int = 0")
+    .Output("costs: float32")
+    .Output("grads: float32")
+    .SetShapeFn([](::tensorflow::shape_inference::InferenceContext* c) {
+        ::tensorflow::shape_inference::ShapeHandle input;
+        TF_RETURN_IF_ERROR(c->WithRank(c->input(0), 4, &input));
+        c->set_output(0, c->Vector(c->Dim(c->input(0), 0)));
+        c->set_output(1, input);
+        return tf::Status::OK();
+    });
+
+
+namespace warp_rna {
+
+const char* rnaGetStatusString(rnaStatus_t status) {
+    switch (status) {
+    case RNA_STATUS_SUCCESS:
+        return "no error";
+    case RNA_STATUS_WARP_FAILED:
+        return "warp failed";
+    case RNA_STATUS_GRADS_BLANK_FAILED:
+        return "kernel launch failed: grads of blank";
+    case RNA_STATUS_GRADS_LABEL_FAILED:
+        return "kernel launch failed: grads of labels";
+    case RNA_STATUS_COSTS_FAILED:
+        return "kernel launch failed: costs";
+    default:
+        return "unknown error";
+
+    }
+
+}
+
+// Currently there is only a GPU version.
+
+#ifdef WARPRNA_ENABLE_GPU
+
+class WarpRNAOpGPU : public tf::OpKernel {
+  public:
+    explicit WarpRNAOpGPU(tf::OpKernelConstruction* ctx) : tf::OpKernel(ctx) {
+        OP_REQUIRES_OK(ctx, ctx->GetAttr("blank_label", &blank_label_));
+    }
+
+    void Compute(tf::OpKernelContext* ctx) {
+        const tf::Tensor* log_probs = nullptr;
+        const tf::Tensor* labels = nullptr;
+        const tf::Tensor* label_lengths = nullptr;
+        const tf::Tensor* input_lengths = nullptr;
+        OP_REQUIRES_OK(ctx, ctx->input("log_probs", &log_probs));
+        OP_REQUIRES_OK(ctx, ctx->input("labels", &labels));
+        OP_REQUIRES_OK(ctx, ctx->input("label_lengths", &label_lengths));
+        OP_REQUIRES_OK(ctx, ctx->input("input_lengths", &input_lengths));
+
+
+        OP_REQUIRES(ctx, log_probs->shape().dims() == 4,
+                    tf::errors::InvalidArgument("log_probs is not a 4-Tensor"));
+        OP_REQUIRES(ctx, labels->shape().dims() == 2,
+                    tf::errors::InvalidArgument("labels is not a 2-Tensor"));
+        OP_REQUIRES(ctx, tf::TensorShapeUtils::IsVector(label_lengths->shape()),
+                     tf::errors::InvalidArgument("label_lengths is not a vector"));
+        OP_REQUIRES(ctx, tf::TensorShapeUtils::IsVector(input_lengths->shape()),
+                     tf::errors::InvalidArgument("input_lengths is not a vector"));
+
+        const auto& log_probs_shape = log_probs->shape();
+        const auto batch_size = log_probs_shape.dim_size(0);
+        const auto max_time = log_probs_shape.dim_size(1);
+        const auto max_u = log_probs_shape.dim_size(2);
+        const auto num_classes_raw = log_probs_shape.dim_size(3);
+
+        auto log_probs_t = log_probs->tensor<float, 4>();
+        auto labels_t = labels->tensor<int32_t, 2>();
+
+        OP_REQUIRES(
+                ctx, tf::FastBoundsCheck(num_classes_raw, std::numeric_limits<int>::max()),
+                tf::errors::InvalidArgument("num_classes cannot exceed max int"));
+
+        OP_REQUIRES(
+                ctx, batch_size == input_lengths->dim_size(0),
+                tf::errors::InvalidArgument("len(input_lengths) != batch_size.  ",
+                                            "len(input_length):  ", input_lengths->dim_size(0),
+                                            " batch_size: ", batch_size));
+        auto input_lengths_t = input_lengths->vec<int>();
+
+        OP_REQUIRES(
+                ctx, batch_size == label_lengths->dim_size(0),
+                tf::errors::InvalidArgument("len(label_lengths) != batch_size.  ",
+                                            "len(label_length):  ", label_lengths->dim_size(0),
+                                            " batch_size: ", batch_size));
+        auto label_lengths_t = label_lengths->vec<int>();
+
+
+        tf::Tensor* costs = nullptr;
+        OP_REQUIRES_OK(ctx, ctx->allocate_output("costs", input_lengths->shape(), &costs));
+        auto costs_t = costs->vec<float>();
+
+        tf::Tensor* grads = nullptr;
+        OP_REQUIRES_OK(ctx, ctx->allocate_output("grads", log_probs->shape(), &grads));
+        auto grads_t = grads->tensor<float, 4>();
+
+        // For this we need the HostMemory() call for label_lengths.
+        Eigen::Tensor<int, 0, Eigen::RowMajor> min_u = label_lengths_t.minimum();
+        int max_s = max_time - min_u() + 1;
+
+        auto counts_shape = tf::TensorShape{batch_size, max_u, 2};
+        tf::Tensor counts;
+        OP_REQUIRES_OK(ctx, ctx->allocate_temp(tf::DT_UINT32, counts_shape, &counts));
+        auto counts_t = counts.tensor<unsigned int, 3>();
+
+        // for both alphas and betas
+        auto buffer_shape = tf::TensorShape{batch_size, max_s, max_u};
+        tf::Tensor alphas, betas;
+        OP_REQUIRES_OK(ctx, ctx->allocate_temp(tf::DT_FLOAT, buffer_shape, &alphas)); // TODO: check error
+        OP_REQUIRES_OK(ctx, ctx->allocate_temp(tf::DT_FLOAT, buffer_shape, &betas)); // TODO: check error
+        auto alphas_t = alphas.tensor<float, 3>();
+        auto betas_t = betas.tensor<float, 3>();
+
+        auto cuda_stream = ctx->eigen_gpu_device().stream();
+        auto rna_status = run_warp_rna(cuda_stream, counts_t.data(), alphas_t.data(), betas_t.data(), labels_t.data(),
+                                  log_probs_t.data(), grads_t.data(), costs_t.data(),
+                                  input_lengths_t.data(), label_lengths_t.data(),
+                                  batch_size, max_time, max_s, max_u, num_classes_raw, blank_label_);
+        OP_REQUIRES(ctx, rna_status == RNA_STATUS_SUCCESS,
+                    tf::errors::Internal("warp_rna error in compute_rna_loss:",
+                                         rnaGetStatusString(rna_status)));
+    }
+
+  private:
+      int blank_label_;
+};
+
+REGISTER_KERNEL_BUILDER(Name("WarpRNA").Device(::tensorflow::DEVICE_GPU)
+                                       .HostMemory("label_lengths"),
+                        WarpRNAOpGPU);
+#undef EIGEN_USE_GPU
+#endif
+
+}
---
+++ tensorflow_binding/warp_rna/__init__.py
@@ -0,0 +1,52 @@
+"""
+copied from https://github.com/HawkAaron/warp-transducer/blob/master/tensorflow_binding/warprnnt_tensorflow/__init__.py
+Changes: RNN-T -> RNA.
+"""
+import imp
+import tensorflow as tf
+from tensorflow.python.framework import ops
+
+lib_file = imp.find_module('kernels', __path__)[1]
+_warprna = tf.load_op_library(lib_file)
+
+
+def rna_loss(acts, labels, input_lengths, label_lengths, blank_label=0):
+    '''Computes the RNA loss between a sequence of activations and a
+    ground truth labeling.
+    Args:
+        acts: A 4-D Tensor of floats.  The dimensions
+                     should be (B, T, U, V), where B is the minibatch index,
+                     T is the time index, U is the prediction network sequence
+                     length, and V indexes over activations for each
+                     symbol in the alphabet.
+        labels: A 2-D Tensor of ints, a padded label sequences to make sure
+                     labels for the minibatch are same length.
+        input_lengths: A 1-D Tensor of ints, the number of time steps
+                       for each sequence in the minibatch.
+        label_lengths: A 1-D Tensor of ints, the length of each label
+                       for each example in the minibatch.
+        blank_label: int, the label value/index that the RNA
+                     calculation should use as the blank label
+    Returns:
+        1-D float Tensor, the cost of each example in the minibatch
+        (as negative log probabilities).
+    * This class performs the softmax operation internally.
+    * The label reserved for the blank symbol should be label 0.
+    '''
+    loss, _ = _warprna.warp_rna(acts, labels, input_lengths,
+                                label_lengths, blank_label)
+    return loss
+
+
+@ops.RegisterGradient("WarpRNA")
+def _RNALossGrad(op, grad_loss, _):
+    grad = op.outputs[1]
+    # NOTE since here we are batch first, cannot use _BroadcastMul
+    grad_loss = tf.reshape(grad_loss, (-1, 1, 1, 1))
+    return [grad_loss * grad, None, None, None]
+
+@ops.RegisterShape("WarpRNA")
+def _RNALossShape(op):
+    inputs_shape = op.inputs[0].get_shape().with_rank(4)
+    batch_size = inputs_shape[0]
+    return [batch_size, inputs_shape]
